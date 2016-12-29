be03ec1b99d38a77db27273a71da59d28105d1a5
Wildan Kurniadi <danprogramming10@gmail.com>
2016-12-29 12:05:47 +0700

    Create Apple.java

---
+++ Java Beginner/javaBeginner_22/Apple.java
@@ -0,0 +1,25 @@
+package youtube;
+
+
+/**
+ *
+ * @author DAN
+ */
+
+public class Apple { 
+    //contoh overloading method
+    public static void hitung(){
+        System.out.println("Ada yang bisa saya hitung?");
+    }
+    
+    public static void hitung(int sisi){        
+        double luas = Math.pow(sisi,2);
+        System.out.println("Bangun Persegi, Luasnya = "+luas);
+    }
+    
+    public static void hitung(int panjang,int lebar){        
+        double luas = panjang * lebar;
+        System.out.println("Bangun Persegi Panjang, Luasnya = "+luas);
+    }
+    
+}
