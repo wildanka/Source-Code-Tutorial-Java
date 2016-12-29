0cdf2c0e54e9b8f997254f9b9faca618aa3d59e5
Wildan Kurniadi <danprogramming10@gmail.com>
2016-12-29 11:49:54 +0700

    Add files via upload

---
+++ Java Beginner/javaBeginner_21/Apple.java
@@ -0,0 +1,21 @@
+
+package youtube;
+
+
+/**
+ *
+ * @author DAN
+ */
+
+public class Apple { 
+    
+    public void a(){
+        System.out.println("Saya method non-static");
+    }
+    
+    public static void b(String nama){        
+        System.out.println("Saya method static"+nama);
+    }
+    
+    
+}
