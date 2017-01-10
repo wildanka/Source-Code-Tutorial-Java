8e39f9f934f92b2b81bfa65318d68c29994c5655
Wildan Kurniadi <danprogramming10@gmail.com>
2017-01-10 14:25:20 +0700

    Create Pen.java

---
+++ Java Beginner/javaBeginner_23/Pen.java
@@ -0,0 +1,18 @@
+package youtube;
+
+
+
+/**
+ *
+ * @author DAN
+ */
+public class Pen {
+    public static void main(String args[]){
+        Manusia orang = new Manusia("Wildan", 21);
+        Manusia orang2 = new Manusia("Arif", 17);
+        
+        orang.perkenalan();
+        orang2.perkenalan();
+        
+    }        
+}
