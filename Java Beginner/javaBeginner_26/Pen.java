c0295bb2b5c92a780baf817d9b3a542dd8851d60
Wildan Kurniadi <danprogramming10@gmail.com>
2017-02-21 02:23:39 +0700

    Update Pen.java

---
+++ Java Beginner/javaBeginner_26/Pen.java
@@ -0,0 +1,23 @@
+package youtube;
+
+
+/**
+ *
+ * @author DAN
+ */
+public class Pen {
+    public static void main(String args[]){
+        //pembuatan array
+        int usia[] = new int[5];
+        
+        //input elemen array
+        usia[0] = 5;
+        usia[1] = 17; 
+        usia[2] = 25;
+        usia[3] = 65;
+        usia[4] = 22;
+        
+        
+        System.out.println(usia[3]);
+    }        
+}
