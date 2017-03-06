4dc023fbf35b15892a5509493b43dcfd195181d1
Wildan Kurniadi <danprogramming10@gmail.com>
2017-03-06 22:57:55 +0700

    Create Pen.java
    Mencari nilai minimum pada array usia dan mencari nilai rata-rata dari array usia

---
+++ Java Beginner/javaBeginner_27/Pen.java
@@ -0,0 +1,49 @@
+package youtube;
+
+
+/**
+ *
+ * @author DAN
+ */
+public class Pen {
+    public static void main(String args[]){
+        //deklarasi variabel
+        double totalUsia; 
+        double rata2;        
+        int usiaMin;
+        
+        //inisialisasi variabel
+        usiaMin = 999;
+        totalUsia = 0;
+        
+        //pembuatan array
+        int usia[] = new int[5];
+        
+        //input elemen array
+        usia[0] = 5;
+        usia[1] = 17; 
+        usia[2] = 25;
+        usia[3] = 65;
+        usia[4] = 22;        
+        
+        // rata = total Nilai / n (banyaknya data)
+        for (int i = 0; i < usia.length; i++) {
+            totalUsia = totalUsia + usia[i];
+        }       
+        rata2 = totalUsia / usia.length;
+        
+        //tampilkan nilai usia rata2
+        System.out.println(String.valueOf(rata2));
+        
+        
+        
+        //mencari nilai minimum
+        for (int i = 0; i < usia.length; i++) {
+            if (usia[i] < usiaMin) {
+                usiaMin = usia[i];
+            }
+        }
+        //tampilkan nilai usia minimum
+        System.out.println(String.valueOf(usiaMin));
+    }        
+}
