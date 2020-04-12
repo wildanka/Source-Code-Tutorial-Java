576a594ba6967ed01f6b069d9e19e9661692b056
Wildan Kurniadi <danprogramming10@gmail.com>
2020-04-12 12:39:55 -0700

    JavaBeginner_34 keyword Super pada Java

---
+++ Java Beginner/JavaBeginner_34_Super/BelajarInheritance/src/main/java/com/mycompany/belajarinheritance/sepeda/Balapan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.belajarinheritance.sepeda;
+
+/**
+ *
+ * @author hp
+ */
+public class Balapan {
+    public void tampilkanRodaSepeda(){
+        Sepeda spd = new Sepeda();
+        System.out.println(spd.roda);
+    }
+}
---
+++ Java Beginner/JavaBeginner_34_Super/BelajarInheritance/src/main/java/com/mycompany/belajarinheritance/sepeda/Sepeda.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.belajarinheritance.sepeda;
+
+/**
+ *
+ * @author hp
+ */
+public class Sepeda {
+    protected int roda = 2;
+    protected int rantai = 1;
+    protected int jok = 1;
+    
+    public void tampilkanJumlahJok(){
+        System.out.println("Jumlah Jok Sepeda : "+jok);
+    }
+    public void berjalan(String namaSepeda){
+        System.out.println("Sepeda "+namaSepeda+" Berjalan...");
+    }
+    
+    public void mengerem(String namaSepeda){
+        System.out.println("Sepeda "+namaSepeda+" Mengerem.. ");
+    }
+    
+    public void belokKanan(String namaSepeda){
+        System.out.println("Sepeda "+namaSepeda+" berbelok ke Kanan ");
+    }
+    
+    public void belokKiri(String namaSepeda){
+        System.out.println("Sepeda "+namaSepeda+" berbelok ke Kiri ");
+    }
+}
